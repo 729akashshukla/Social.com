40cc9c382d49dd1d5f620ad65ed8493166302865
729akashshukla <729akashshukla@gmail.com>
2025-02-05 02:47:44 +0530

    intial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <h1 className="text-3xl font-bold underline bg-blue-500">hello from tailwind css</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
